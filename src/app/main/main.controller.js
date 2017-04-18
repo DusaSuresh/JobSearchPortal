dd1728697422d4a4751c32255c3092465640ab90
Suresh <dusa.suresh@gmail.com>
2017-04-18 20:30:20 +0800

    New google map with info window

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -15,8 +15,97 @@
     vm.jobCategories = SearchService.getJobCategory();
     vm.jobClasstypes = SearchService.getJobClasstype();
 
+    //Google map
+
+    $window.initMap = function() {
+        var uluru = {lat: 42.407211, lng: -71.382439};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+        setMarkers(map);
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+      }
+
+var beaches = [
+        ['Boston city', 42.332221, -71.016432, 4],
+        ['Brookline', 42.324269, -71.140803, 5],
+        ['Framingham', 42.308536, -71.436754, 3],
+        ['Malden city', 42.430474, -71.057637, 2],
+        ['Waltham city', 42.38892, -71.242325, 1]
+      ];
+
+
+      function setMarkers(map) {
+        // Adds markers to the map.
+
+        // Marker sizes are expressed as a Size of X,Y where the origin of the image
+        // (0,0) is located in the top left of the image.
+
+        // Origins, anchor positions and coordinates of the marker increase in the X
+        // direction to the right and in the Y direction down.
+        var image = {
+          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          // This marker is 20 pixels wide by 32 pixels high.
+          size: new google.maps.Size(20, 32),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(0, 32)
+        };
+        // Shapes define the clickable region of the icon. The type defines an HTML
+        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+        // The final coordinate closes the poly by connecting to the first coordinate.
+        var shape = {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        };
+        for (var i = 0; i < beaches.length; i++) {
+          var beach = beaches[i];
+          var marker = new google.maps.Marker({
+            position: {lat: beach[1], lng: beach[2]},
+            map: map,
+            icon: image,
+            shape: shape,
+            title: beach[0],
+            zIndex: beach[3]
+          });
+          
+          marker.addListener('click', function() {
+            var infowindow = new google.maps.InfoWindow({
+          content: "suresh"
+        });
+          infowindow.open(map, marker);
+        });
+        }
+      }
   // Displaying map on landing page using Datamap api and setting configuration of map
-  var election = new Datamap({
+  /*var election = new Datamap({
   scope: 'usa',
   element: $document[0].querySelector('#container'),
   done: function(datamap) {
@@ -244,7 +333,7 @@ data:{
 }
 });
 election.labels();
-          
+  */        
     activate();
 
 
